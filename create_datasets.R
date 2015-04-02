4d284d1d1ed321296003d6c1ff94f748257f9d2a
Learn2Mine <clayton_turner@bitbucket.org>
2015-04-01 22:27:55 -0400

    Chagnes.

--- create_datasets.R
+++ create_datasets.R
@@ -4,6 +4,8 @@ df = read.table('collection_All_,_PVT_Scores_9481.txt',skip=14)
 ppm = df[,1]
 X = df[,2:ncol(df)]
 
+all.maxs = T
+
 plot.metabolite <- function(dir,metabolite,part,X,inxs) {
   X.m = X[inxs,]
   data = cbind(ppm[inxs],X.m)
@@ -19,20 +21,20 @@ plot.metabolite <- function(dir,metabolite,part,X,inxs) {
   }
 }
 
-metabolite = 'Taurine'
-regions = matrix(c(3.5,3.2),ncol=2)
-inxs = c()
-for (i in 1:nrow(regions)) {
-  inxs = c(inxs,which(regions[i,1] >= ppm & ppm >= regions[i,2]))
-}
-plot.metabolite(metabolite,X,inxs)
-
-metabolite = 'alpha-ketoglutarate'
-regions = matrix(c(3.1,2.9,2.5,2.35),ncol=2,byrow=T)
-for (i in 1:nrow(regions)) {
-  inxs = which(regions[i,1] >= ppm & ppm >= regions[i,2])
-  plot.metabolite(metabolite,paste(metabolite,'-',i,'-',sep=""),X,inxs,c(0,0.5))
-}
+# metabolite = 'Taurine'
+# regions = matrix(c(3.5,3.2),ncol=2)
+# inxs = c()
+# for (i in 1:nrow(regions)) {
+#   inxs = c(inxs,which(regions[i,1] >= ppm & ppm >= regions[i,2]))
+# }
+# plot.metabolite(metabolite,X,inxs)
+# 
+# metabolite = 'alpha-ketoglutarate'
+# regions = matrix(c(3.1,2.9,2.5,2.35),ncol=2,byrow=T)
+# for (i in 1:nrow(regions)) {
+#   inxs = which(regions[i,1] >= ppm & ppm >= regions[i,2])
+#   plot.metabolite(metabolite,paste(metabolite,'-',i,'-',sep=""),X,inxs,c(0,0.5))
+# }
 
 metabolite = 'Tyrosine'
 regions = matrix(c(7.25,7.15,6.95,6.85,4,3.85,3.25,3.15,3.1,3),ncol=2,byrow=T)
@@ -43,7 +45,11 @@ regions = matrix(c(7.25,7.15,6.95,6.85,4,3.85,3.25,3.15,3.1,3),ncol=2,byrow=T)
 inxs = c()
 for (i in 1:nrow(regions)) {
   region.inxs = which(regions[i,1] >= ppm & ppm >= regions[i,2])
-  maxs = apply(X[region.inxs,],2,max)
+  if (all.maxs == F) {
+    maxs = apply(X[region.inxs,],2,max)
+  } else {
+    maxs = apply(X,2,max)    
+  }
   for (j in 1:ncol(X)) {
     X[region.inxs,j] = X[region.inxs,j]/maxs[j]
   }
@@ -53,8 +59,11 @@ X.m = X[inxs,]
 data = cbind(ppm[inxs],X.m)
 colnames(data)[1] = 'ppm'
 colnames(data)[2:ncol(data)] = 1:(ncol(data)-1)
-write.csv(data,file='Tyrosine/positive_train.csv',quote=F,row.names=F)
-
+if (all.maxs == F) {
+  write.csv(data,file='Tyrosine/positive_train.csv',quote=F,row.names=F)
+} else {
+  write.csv(data,file='Tyrosine/positive_train_global_max.csv',quote=F,row.names=F)  
+}
 inxs = c()
 regions = matrix(c(7.25,7.15,6.95,6.85,4,3.85,3.25,3.15,3.1,3)+1,ncol=2,byrow=T)
 for (i in 1:nrow(regions)) {
@@ -69,4 +78,8 @@ X.m = X[inxs,]
 data = cbind(ppm[inxs]+1,X.m)
 colnames(data)[1] = 'ppm'
 colnames(data)[2:ncol(data)] = 1:(ncol(data)-1)
-write.csv(data,file='Tyrosine/negative_train.csv',quote=F,row.names=F)
+if (all.maxs == F) {
+  write.csv(data,file='Tyrosine/negative_train.csv',quote=F,row.names=F)
+} else {
+  write.csv(data,file='Tyrosine/negative_train_global_max.csv',quote=F,row.names=F)  
+}
